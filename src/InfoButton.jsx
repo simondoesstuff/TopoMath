f6abb3eba6f635cafbd2081ed79d7650723be3c3
simondoesstuff <sw.simonwalker@gmail.com>
2021-11-14 01:19:27 -0700

    started implementing info button

---
+++ src/InfoButton.jsx
@@ -0,0 +1,9 @@
+import {ReactComponent as Icon} from "./svgFiles/info-button.svg";
+
+export default function InfoButton() {
+    return (
+        <a href='#!' className="info-button">
+            <Icon/>
+        </a>
+    )
+}
