d100fad0b2c2fe8f8a5c9b9ccfd6988228a70540
simondoesstuff <sw.simonwalker@gmail.com>
2022-02-02 15:47:22 -0700

    ExpressionBar now has a pull up/down tab

--- src/components/pages/index/ExpressionBar/ExpressionBar.svelte
+++ src/components/pages/index/ExpressionBar/ExpressionBar.svelte
@@ -1,14 +1,55 @@
 <script>
   import EditableMathField from "../../../mathquill/EditableMathField.svelte";
+  import {fly} from 'svelte/transition';
+
+
+  let contracted = false;
 </script>
 
 
 
 
-<!--                todo how do shadow??????????????????       -->
-<EditableMathField
-        on:latexedit
-        class="shadow shadow-lg shadow-black"
->
-    f(x)
-</EditableMathField>
+
+{#if !contracted}
+    <div transition:fly={{y: -100}}>
+        <div class="grid place-items-center p-3 text-5xl text-white bg-indigo-900">
+            <EditableMathField on:latexedit>
+                f(x)
+            </EditableMathField>
+        </div>
+
+
+        <div
+                on:click={() => contracted = !contracted}
+                class="ribbon opacity-80"
+        >
+        </div>
+    </div>
+{/if}
+
+{#if contracted}
+    <div
+            in:fly={{delay: 400, y: -50}}
+            on:click={() => contracted = !contracted}
+            class="opacity-30 ribbon"
+    >
+    </div>
+{/if}
+
+
+
+
+
+<style>
+    .ribbon {
+        @apply absolute right-1/4 w-12 h-4 bg-gray-400;
+
+        transition: height ease-out;
+        transition-duration: 300ms;
+        border-radius: 50% 50% 50% 50% / 0% 0% 40% 40%;
+    }
+
+    .ribbon:hover {
+        @apply h-8;
+    }
+</style>
