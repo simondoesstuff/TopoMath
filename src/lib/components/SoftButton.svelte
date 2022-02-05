4e6b59371dac98e76fb0c96ce207b515f904b5b2
simondoesstuff <sw.simonwalker@gmail.com>
2022-02-04 22:56:39 -0700

    new button component with "soft" styling

---
+++ src/lib/components/SoftButton.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let clazz;
+</script>
+
+
+
+
+<div class={`grid place-items-center ${clazz}`}>
+    <button on:click class="
+            p-2 bg-gray-800 bg-opacity-95 rounded-3xl border-2 border-indigo-500 border-opacity-60 opacity-70 border-solid
+            font-semibold text-gray-300
+            transition-colors duration-500 cursor-pointer block
+            hover:bg-indigo-500 hover:border-transparent hover:opacity-100"
+    >
+        <slot></slot>
+    </button>
+</div>
