e0c20bd19cce559c30ca44f5f67a8e5286de9d04
simondoesstuff <sw.simonwalker@gmail.com>
2022-02-04 22:58:26 -0700

    added new buttons. touched up "ribbon" button. preparing for a new control bar UI with a menu button that animates the whole bar into a settings section.

--- src/lib/components/pages/index/ControlBar/ControlBar.svelte
+++ src/lib/components/pages/index/ControlBar/ControlBar.svelte
@@ -2,6 +2,10 @@
   import EditableMathField from "../../../mathquill/EditableMathField.svelte";
   import {fly} from 'svelte/transition';
   import {createEventDispatcher} from "svelte";
+  import SoftButton from "$lib/components/SoftButton.svelte";
+  import MenuIcon from '$lib/svgIcons/Menu.svelte'
+  import Minus from "$lib/svgIcons/Minus.svelte";
+  import Plus from "$lib/svgIcons/Plus.svelte";
 
 
   let latex = 'f(x)';
@@ -26,15 +30,30 @@
 
 {#if !contracted}
     <div transition:fly={{y: -100}}>
-        <div class="grid place-items-center p-3 text-5xl text-white bg-indigo-900">
-            <EditableMathField on:latexedit={handleLatexEdit}>
+        <div class="flex justify-between p-3 px-6 align-middle bg-indigo-900">
+            <SoftButton>
+                <MenuIcon/>
+            </SoftButton>
+            <EditableMathField
+                    on:latexedit={handleLatexEdit}
+                    clazz="grid place-items-center text-6xl text-white"
+            >
                 {latex}
             </EditableMathField>
+
+            <div>
+                <SoftButton clazz="mb-2">
+                    <Plus/>
+                </SoftButton>
+                <SoftButton>
+                    <Minus/>
+                </SoftButton>
+            </div>
         </div>
 
         <div
                 on:click={() => contracted = !contracted}
-                class="ribbon opacity-80"
+                class="opacity-60 ribbon"
         >
         </div>
     </div>
@@ -44,7 +63,7 @@
     <div
             in:fly={{delay: 400, y: -50}}
             on:click={() => contracted = !contracted}
-            class="opacity-30 ribbon"
+            class="opacity-25 ribbon"
     >
     </div>
 {/if}
@@ -59,10 +78,10 @@
 
         transition: height ease-out;
         transition-duration: 300ms;
-        border-radius: 50% 50% 50% 50% / 0% 0% 40% 40%;
+        border-radius: 50% 50% 50% 50% / 0 0 40% 40%;
     }
 
     .ribbon:hover {
-        @apply h-8;
+        @apply h-8 bg-gray-300 opacity-100;
     }
 </style>
