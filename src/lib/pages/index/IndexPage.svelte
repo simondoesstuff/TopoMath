aa88db160804ddcbff6c8083f14261938154d807
simondoesstuff <sw.simonwalker@gmail.com>
2022-03-07 15:27:21 -0700

    now a typescript + svelte project

--- src/lib/pages/index/IndexPage.svelte
+++ src/lib/pages/index/IndexPage.svelte
@@ -1,12 +1,13 @@
 <script>
     import ExpressionBar from "./ControlBar.svelte";
+    import ThreeScene from "../../components/threeJS/ThreeScene.svelte";
 
     let elLog;
 </script>
 
 
 <ExpressionBar on:latexedit={(e) => elLog.log(e.detail.latex)}/>
-
+<ThreeScene/>
 
 <!--    todo remove domLog?     -->
 <!--<div class="absolute top-0 right-0 z-50 mr-3 text-right opacity-40">-->
